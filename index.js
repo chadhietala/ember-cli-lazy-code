121898e0b68afef66131d06409f72c723fd2556d
Chad Hietala <chadhietala@gmail.com>
2016-04-09 11:52:40 -0700

    Init

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /* jshint node: true */
 'use strict';
 
+var LazyCode = require('./lib');
+var stew = require('broccoli-stew');
+var merge = require('broccoli-merge-trees');
+var find = stew.find;
+
 module.exports = {
-  name: 'ember-cli-lazy-code'
+  name: 'ember-cli-lazy-code',
+  included: function(app) {
+    this.app = app;
+  },
+  postprocessTree: function(type, tree) {
+    if (type === 'all') {
+      var app = find(tree, { include: ['assets/' + this.app.name + '.js']});
+      var lazyApp = new LazyCode(app, {
+        description: 'ember-cli-lazy-code'
+      });
+
+      return merge([tree, lazyApp], { overwrite: true });
+    }
+    return tree;
+  }
 };
